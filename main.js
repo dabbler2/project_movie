6c018252522f68aae363620f9a8b644e11229e21
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2023-10-19 12:13:41 +0900

    .

--- main.js
+++ main.js
@@ -6,12 +6,17 @@ const options = {
   }
 };
 
-async function scrap(m=2){
+let cards = document.getElementById('cards')
+let message = document.getElementById('message')
+
+async function scrap(m=3){
 	let proms = [], movieList = []
-	for(let i=1;i<=2;++i) proms.push(await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page='+i, options))
+	message.textContent = "로딩 중입니다. 잠시만 기다려주세요."
+	for(let i=1;i<=m;++i) proms.push(await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page='+i, options))
 	let proms2 = await Promise.all(proms)
 	let jsons = await Promise.all(proms2.map(res => res.json()))
 	let results = await jsons.map(r => r.results.map(c => movieList.push(c)))
+	message.textContent = ""
 	return movieList
 }
 
@@ -52,25 +57,22 @@ let moviecard = movie => {
 	
 	return card
 }
-//
+
 async function main(){
 	let movieList = await scrap()
 	console.log(movieList[0])
 	let searchBtn = document.getElementById('searchBtn')
 	searchBtn.onclick = () => {
 		let keyword = document.getElementById('keyword').value.toUpperCase()
-		let cards = document.getElementById('cards')
+		document.getElementById('keyword').value = ''
 		cards.replaceChildren()
+		if(!keyword || keyword.split(' ').length==keyword.length+1){
+			message.textContent = "검색 키워드를 입력해주세요."; return
+		}message.textContent = keyword+"의 검색 결과"
 		movieList.filter(movie => movie.title.toUpperCase().includes(keyword))
 			.map(movie => cards.appendChild(moviecard(movie)))
-		if(!cards.firstChild) cards.textContent = "검색 결과가 없습니다."
+		if(!cards.firstChild) message.textContent = keyword+"의 검색 결과가 없습니다."
 	}
 }
-
-/* async function main(){
-	let movieList = await scrap()
-	//let a = document.getElementById('a')
-	//for(let j=0;j<movieList.length;++j) a.innerHTML += movieList[j]+'\n'
-}*/
 
 main() 
